22f58b2088a6f120556caa611d51fc1a92bf8471
Tanya Staneva <33036767+tany1610@users.noreply.github.com>
2019-03-10 11:49:51 +0200

    upload pick-the-lighte project

---
+++ Pick-The-Lighter/libraries/mx-math.js
@@ -0,0 +1,142 @@
+class Matrix {
+  constructor(rows, cols, matrix=null) {
+    this.value = [];
+    this.rows = rows || matrix.length;
+    this.cols = cols || matrix[0].length;
+    if (matrix) {
+      for (let row = 0; row < this.rows; row++) {
+        this.value[row] = [];
+        for (let col = 0; col < this.cols; col++) {
+          this.value[row][col] = matrix[row][col];
+          if (!matrix[row][col]) {
+            throw new Error('x and y do not match with the dimentions of the matrix.');
+          }
+        }
+      }
+    } else {
+      for (let row = 0; row < this.rows; row++) {
+        this.value[row] = [];
+        for (let col = 0; col < this.cols; col++) {
+          this.value[row][col] = 1;
+        }
+      }
+    }
+  }
+
+  randomize() {
+    for (let row = 0; row < this.rows; row++) {
+      for (let col = 0; col < this.cols; col++)
+        this.value[row][col] = Math.random() /** 3 + 1*/;
+    }
+  }
+
+  add(x) {
+    if (x instanceof Matrix) {
+      if (this.rows !== x.rows || this.cols !== x.cols) {
+        throw new Error('rows and columns of the matrices must match');
+      }
+      for (let row = 0; row < this.rows; row++) {
+        for (let col = 0; col < this.cols; col++) {
+          this.value[row][col] += x.value[row][col];
+        }
+      }
+    } else if (+x) {
+      for (let row = 0; row < this.rows; row++) {
+        for (let col = 0; col < this.cols; col++) {
+          this.value[row][col] += x;
+        }
+      }
+    } else {
+      throw new Error('added value must either be Matrix or a number');
+    }
+  }
+
+  multiply(x) {
+    if (+x) {
+      for (let row = 0; row < this.rows; row++) {
+        for (let col = 0; col < this.cols; col++) {
+          this.value[row][col] *= x;
+        }
+      }
+    } else {
+      for (let row = 0; row < this.rows; row++) {
+        for (let col = 0; col < this.cols; col++) {
+          this.value[row][col] *= x.value[row][col];
+        }
+      }
+    }
+  }
+
+  map(f) {
+    for (let row = 0; row < this.rows; row++) {
+      for (let col = 0; col < this.cols; col++) {
+        this.value[row][col] = f(this.value[row][col]);
+      }
+    }
+  }
+
+  print() {
+    console.table(this.value);
+  }
+
+  // static functions
+  static fromArray(arr) {
+    let result = new Matrix(arr.length, 1);
+    for (let row = 0; row < arr.length; row++) {
+      for (let col = 0; col < 1; col++) {
+        result.value[row][col] = arr[row];
+      }
+    }
+    return result;
+  }
+
+  static toArray(m) {
+    let result = [];
+    for (let row = 0; row < m.rows; row++) {
+      for (let col = 0; col < m.cols; col++) {
+        result.push(m.value[row][col]);
+      }
+    }
+    return result;
+  }
+
+  static multiply(m1, m2) {
+    let result = new Matrix(m1.rows, m2.cols);
+    for (let row = 0; row < result.rows; row++) {
+      for (let col = 0; col < result.cols; col++) {
+        let sum = 0;
+        for (let k = 0; k < m1.cols; k++) {
+          sum += m1.value[row][k] * m2.value[k][col];
+        }
+        result.value[row][col] = sum;
+      }
+    }
+    return result;
+  }
+
+  static subtract(m1, m2) {
+    let result = new Matrix(m1.rows, m1.cols);
+    for (let row = 0; row < result.rows; row++) {
+      for (let col = 0; col < result.cols; col++) {
+        result.value[row][col] = m1.value[row][col] - m2.value[row][col];
+      }
+    }
+    return result;
+  }
+
+  static transpose(m) {
+    let result = new Matrix(m.cols, m.rows);
+    for (let row = 0; row < result.rows; row++) {
+      for (let col = 0; col < result.cols; col++) {
+        result.value[row][col] = m.value[col][row];
+      }
+    }
+    return result;
+  }
+}
+
+// testing
+// let m1 = new Matrix(2, 1, [[2], [3]]);
+// let m2 = new Matrix(2, 2, [[1, 2], [3, 4]]);
+// m1.add(m2);
+// m1.print();
---
+++ Pick-The-Lighter/sketch.js
@@ -0,0 +1,119 @@
+let left_square_col;
+let right_square_col;
+let reward = 0;
+let totalReward = 0;
+let choice;
+let brain;
+let actions;
+let rateSlider;
+let assuredness;
+
+function setup() {
+  createCanvas(800, 800);
+  rateSlider = createSlider(1, 120, 1, 1);
+  rateSlider.position(250, 130);
+  // creating the neural network
+  brain = new NeuralNetwork(2, 4, 2, 0.1);
+  left_square_col = floor(random(255));
+  right_square_col = floor(random(255));
+
+  // possible actions
+  actions = [left, right];
+
+  // debugging
+  // console.log(output);
+  // console.log(action);
+}
+
+function draw() {
+  background(0);
+  frameRate(rateSlider.value());
+  translate(width / 2, height / 2);
+
+  // creating inputs
+  let x1 = map(left_square_col, 0, 255, 0, 1);
+  let x2 = map(right_square_col, 0, 255, 0, 1);
+
+  // creating the current state
+  let state = [x1, x2];
+
+  // taking an action
+  let choice = brain.predict(state);
+  let chosenAction = choice.indexOf(max(choice));
+  let randomAction = choice.indexOf(random(choice));
+  let action;
+
+  // getting the assuredness of the network
+  assuredness = brain.getAssuredness(choice);
+  console.log(assuredness);
+  if (random(1) < assuredness) {
+    action = chosenAction;
+  } else {
+    action = randomAction;
+  }
+  actions[action]();
+
+  // accumulating reward
+  brain.accumulateReward(state, action, reward);
+
+  // drawing game
+  drawGame();
+
+  // drawing neural network
+  brain.draw(200, -400);
+
+  // next iteration
+  nextEpisode();
+}
+
+function drawGame() {
+  if (choice === 'left') {
+    fill(255);
+    triangle(-10, -10, -10, 20, -35, 5);
+  } else if (choice === 'right') {
+    fill(255);
+    triangle(10, -10, 10, 20, 35, 5);
+  }
+  textSize(32);
+  stroke(255);
+  strokeWeight(0.1);
+  text("Reward: " + reward.toFixed(2), -350, -350);
+  text("Total Reward: " + totalReward.toFixed(2), -350, -300);
+  text("Frame Rate: ", -350, -250);
+  text("Assured of current action: " + (assuredness * 100).toFixed(2) + "%", -350, -200);
+  fill(left_square_col);
+  noStroke();
+  rect(-150, -50, 100, 100);
+  fill(right_square_col);
+  noStroke();
+  rect(50, -50, 100, 100);
+}
+
+function left() {
+  let diff = -(right_square_col - left_square_col);
+  choice = 'left';
+  let mappedDiff = map(diff, -255, 255, -1, 1);
+  if (mappedDiff < 0) {
+    reward = map(mappedDiff, -1, 0, -1, -0.1);
+  } else {
+    reward = map(mappedDiff, 0, 1, 1, 0.7);
+  }
+  totalReward += reward;
+}
+
+function right() {
+  let diff = -(left_square_col - right_square_col);
+  choice = 'right';
+  let mappedDiff = map(diff, -255, 255, -1, 1);
+  if (mappedDiff < 0) {
+    reward = map(mappedDiff, -1, 0, -1, -0.1);
+  } else {
+    reward = map(mappedDiff, 0, 1, 1, 0.7);
+  }
+  totalReward += reward;
+}
+
+function nextEpisode() {
+  left_square_col = floor(random(255));
+  right_square_col = floor(random(255));
+}
